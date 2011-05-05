f2aea83ce87bfbc9e7e59051a6dbfbe67a83a00a
Jere <me@jeresalonen.com>
2011-05-05 02:35:28 +0200

    Basecamp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Disegno::Application.config.secret_token = 'c207c50860c2b90a1b847294b7a36eb76e5555029d4a51cdc1467b3e090442c87e6f38e752096693c1c7bb8ccc4f4b9539faee14a7a7d987d53c8c44263c0927'
